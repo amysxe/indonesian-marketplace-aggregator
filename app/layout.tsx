192208708ef95acff497c0f361426a58735c55de
Naimy Zam Zam <fuxnbums@gmail.com>
2025-09-02 10:40:02 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Cari Produk Inceranmu',
-  description: 'Tulis produk yang kamu cari, pilih yang terbaik',
+  description: 'Tulis produk yang kamu cari, pilih yang terbaik.',
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
