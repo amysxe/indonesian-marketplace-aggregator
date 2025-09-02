8ac3968cb927e47747dec60e813af3070bc60593
Naimy Zam Zam <fuxnbums@gmail.com>
2025-09-02 10:47:06 +0700

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // PostCSS is used to process your CSS. This configuration tells it
+  // to run Tailwind CSS and Autoprefixer, which handles vendor prefixes.
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
